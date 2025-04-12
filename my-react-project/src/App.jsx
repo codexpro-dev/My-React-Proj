30a28565aa8390a338306d0c200d7999ac20b52d
codexpro.dev <chimeziev900@gmail.com>
2025-04-12 13:37:06 -0700

    complete accordion proj

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import {accordionData} from './components/Accordions/utils/content'
+import Accordion from './components/Accordions/Accordion'
 
 const App = () => {
   return (
     <div>
-     <Accordion />
+    <div className="accordion">
+      {accordionData.map(({title, content}) => (
+        <Accordion title={title} content={content} />
+      ))}
+
+    </div>
     </div>
   )
 }
--- my-react-project/src/components/Accordions/Accordion.jsx
+++ my-react-project/src/components/Accordions/Accordion.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-
-const Accordion = () => {
+import React, { useState } from 'react'
+import '../Accordions/accordion.css'
+const Accordion = ({title,content}) => {
+  const [isActive,setIsActive] = useState(false)
   return (
-    <div>
-      
-    </div>
+    <section className='accordion-card' key={Math.random()}>
+      <div className="header" onClick={() => setIsActive(!isActive)}>
+        <div>{title}</div>
+        <p className='icon'>{isActive ? '-' : '+'}</p>
+      </div>
+
+      <div className="content">
+        {isActive && <p className='card-info'>{content}</p>}
+      </div>
+    </section>
   )
 }
 
